fc293850ab1d3a612f5293f3143802f3fab60822
MarcosRabreu <abreubr@gmail.com>
2024-05-24 16:57:21 -0300

    inclusão de template basico

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from . import models
 
-# Create your views here.
 def home(request):
-    return HttpResponse("<h1>hello, world!!!</h1>")
+    return render(request,'blog/home.html')
