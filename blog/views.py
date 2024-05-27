d2a9fb7cb50e15a4931e094997292498766b64bd
MarcosRabreu <abreubr@gmail.com>
2024-05-27 11:05:16 -0300

    criação de uma novo template

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,20 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from . import models
+from .models import Post
 
 def home(request):
-    return render(request,'blog/home.html')
+    posts = Post.objects.all()
+    # posts = Post.objects.get(pk=1)
+    context = {
+        'posts' : posts
+    }
+    return render(request,'blog/home.html', context)
+
+def post_detail(request, post_id):
+    posts = Post.objects.get(pk=post_id)
+
+    context = {
+        'posts' : posts
+    }
+    return render(request,'blog/post_detail.html', context)
+
+
